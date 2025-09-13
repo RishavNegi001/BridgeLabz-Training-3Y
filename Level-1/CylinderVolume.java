9f9cda9e920ffcda9e4c6760019fa94e63c20a87
Rishav Negi <rishunegi077@gmail.com>
2025-09-13 23:52:26 +0530

    [Rishav] add : added solutions

---
+++ Level-1/CylinderVolume.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CylinderVolume {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double radius = input.nextDouble();
+
+        System.out.print("Enter height: ");
+        double height = input.nextDouble();
+
+        double volume = Math.PI * radius * radius * height;
+
+        System.out.println("The volume of the cylinder is " + volume);
+    }
+}
